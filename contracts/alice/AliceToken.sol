62ec4d75e14efa9cd512cea42bcc0242727441eb
Jakub Wojciechowski <jakub@alice.si>
2017-07-04 17:29:17 +0100

    Standard Zepellin token

--- contracts/alice/AliceToken.sol
+++ contracts/alice/AliceToken.sol
@@ -6,8 +6,11 @@ pragma solidity ^0.4.11;
 
 import "./Token.sol";
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
-contract AliceToken is Token, Ownable {
+contract AliceToken is StandardToken, Ownable {
+    using SafeMath for uint256;
 
     string public name = "Alice Token";
     uint8 public decimals = 2;
@@ -15,56 +18,19 @@ contract AliceToken is Token, Ownable {
     string public version = 'ALT 1.0';
 
 
-    function transfer(address _to, uint256 _value) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-        } else { throw; }
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-        } else { throw; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
-
     function mint(address _to, uint256 _value) onlyOwner {
-        if (totalSupply + _value < totalSupply) throw;
-            totalSupply += _value;
-            balances[_to] += _value;
+        totalSupply = totalSupply.add(_value);
+        balances[_to] =  balances[_to].add(_value);
 
-            MintEvent(_to, _value);
+        MintEvent(_to, _value);
     }
 
     function destroy(address _from, uint256 _value) onlyOwner {
-        if (balances[_from] < _value || _value < 0) throw;
-            totalSupply -= _value;
-            balances[_from] -= _value;
+        totalSupply = totalSupply.sub(_value);
+        balances[_from] = balances[_from].sub(_value);
 
-            DestroyEvent(_from, _value);
+        DestroyEvent(_from, _value);
     }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
 
     event MintEvent(address indexed to, uint value);
     event DestroyEvent(address indexed from, uint value);
