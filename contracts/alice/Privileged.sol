5117536e5514c1a9e964a18ca3feea23d2408322
jakub-wojciechowski <jakub@alice.si>
2017-10-12 17:49:50 +0200

    Privileged contract.

---
+++ contracts/alice/Privileged.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.11;
+
+
+/**
+ * @title Privileged
+ * @dev The Privileged defines a set of privileges for a smart contract
+ */
+contract Privileged {
+    address public owner;
+
+
+    event PrivilegeTransferred(string privilege, address indexed previousOwner, address indexed newOwner);
+
+    struct Privilege {
+      address owner;
+      address controller;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the privilege owner.
+     */
+    modifier only(string _privilege) {
+        require(msg.sender == privileges[_privilege].owner);
+        _;
+    }
+
+
+    mapping(string => Privilege) privileges;
+
+
+    function createPrivilege(string _privilegeName, address _owner, address _controller) {
+        require(privileges[_privilegeName].owner == address(0));
+        privileges[_privilegeName] = Privilege(_owner, _controller);
+        PrivilegeTransferred(_privilegeName, address(0), _owner);
+    }
+
+
+    function revokePrivilege(string _privilegeName) {
+        require(msg.sender == privileges[_privilegeName].controller);
+        PrivilegeTransferred(_privilegeName, privileges[_privilegeName].owner, address(0));
+        privileges[_privilegeName] = Privilege(0, 0);
+    }
+
+
+    /**
+     * @dev Allows the privilege controller to transfer ownership of a privilege to another account.
+     * @param _newOwner The address to transfer privilege ownership to.
+     */
+    function transferPrivilege(string _privilege, address _newOwner) public {
+        require(msg.sender == privileges[_privilege].controller);
+        require(_newOwner != address(0));
+        privileges[_privilege].owner = _newOwner;
+        PrivilegeTransferred(_privilege, owner, _newOwner);
+    }
+
+}
---
+++ test/priviledged.js
@@ -0,0 +1,52 @@
+var Privileged     = artifacts.require("Privileged");
+
+const BigNumber = web3.BigNumber
+
+const should = require('chai')
+	.use(require('chai-as-promised'))
+	.use(require('chai-bignumber')(BigNumber))
+	.should()
+
+contract('Singe impactRegistry donation', function(accounts) {
+	var owner = accounts[0];
+	var controller = accounts[1];
+	var owner2 = accounts[2];
+	var privileged;
+
+	beforeEach("deploy Privileged contract", async function() {
+		privileged = await Privileged.new();
+	});
+
+	it("should create privilege", async function() {
+		await privileged.createPrivilege("owner", owner, owner);
+	});
+
+	it("shouldn't allow creating the same privilege", async function() {
+		await privileged.createPrivilege("owner", owner, owner);
+		await privileged.createPrivilege("owner", owner, owner).should.be.rejectedWith('invalid opcode');
+	});
+
+	it("should transfer ownership as controller", async function() {
+		await privileged.createPrivilege("owner", owner, controller);
+		await privileged.transferPrivilege("owner", owner2, {from: controller});
+	});
+
+	it("shouldn't allow transferring ownership as owner", async function() {
+		await privileged.createPrivilege("owner", owner, controller);
+		await privileged.transferPrivilege("owner", owner2, {from: owner}).should.be.rejectedWith('invalid opcode');
+	});
+
+	it("should revoke and recreate a privilege", async function() {
+		await privileged.createPrivilege("owner", owner, controller);
+		await privileged.revokePrivilege("owner", {from: controller});
+		await privileged.createPrivilege("owner", owner, controller);
+	});
+
+	it("shouldn't allow revoking if it's not a controller", async function() {
+		await privileged.createPrivilege("owner", owner, controller);
+		await privileged.revokePrivilege("owner", {from: owner}).should.be.rejectedWith('invalid opcode');
+	});
+
+
+
+});
