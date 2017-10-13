86fcb8891574607c00f9b137a9977e085422cd08
jakub-wojciechowski <jakub@alice.si>
2017-10-13 17:30:01 +0200

    DonationWallet

---
+++ contracts/alice/DonationWallet.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/token/ERC20.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import './Project.sol';
+
+contract DonationWallet is Ownable {
+    using SafeMath for uint256;
+
+
+  function donate(ERC20 _token, uint _amount, Project _project) onlyOwner {
+    _token.approve(address(_project), _amount);
+  }
+
+  function refund(ERC20 _token, address _destination, uint _amount) onlyOwner {
+    _token.transfer(_destination, _amount);
+  }
+
+  function balance(ERC20 _token) constant returns(uint256){
+    return _token.balanceOf(this);
+  }
+
+}
