4f958a895e82d3860607bed538b0cd51aa4c8c30
jakub-wojciechowski <jakub@alice.si>
2017-10-16 15:44:07 +0200

    Escapable tests.

--- test/escapable.js
+++ test/escapable.js
@@ -11,10 +11,11 @@ const should = require('chai')
 contract('ProjectCatalog', function(accounts) {
 	var escapeController = accounts[1];
 	var escapeTarget = accounts[2];
+	var newController = accounts[3];
 	var escapable;
 	var token;
 
-	before("deploy Escapable and deposit tokens", async function() {
+	beforeEach("deploy Escapable and deposit tokens", async function() {
 		escapable = await Escapable.new(escapeController, escapeTarget);
 		token = await AliceToken.deployed();
 		await token.mint(escapable.address, 100);
@@ -22,6 +23,23 @@ contract('ProjectCatalog', function(accounts) {
 
 	it("should correctly deposit tokens", async function() {
 		(await token.balanceOf(escapable.address)).should.be.bignumber.equal(100);
+	});
+
+	it("should prevent escaping from non controller", async function() {
+		escapable.escape(token.address, {from: newController}).should.be.rejectedWith('invalid opcode');
+	});
+
+	it("should allow changing controller", async function() {
+		await escapable.changeEscapeController(newController, {from: escapeController});
+
+		(await escapable.escapeController()).should.be.equal(newController);
+	});
+
+	it("should escape funds", async function() {
+		await escapable.escape(token.address, {from: escapeController});
+
+		(await token.balanceOf(escapable.address)).should.be.bignumber.equal(0);
+		(await token.balanceOf(escapeTarget)).should.be.bignumber.equal(100);
 	});
 
 });
