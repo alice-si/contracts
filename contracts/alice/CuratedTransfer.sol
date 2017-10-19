fca64dc4c434795eb0d501cb857eb611546e6b37
jakub-wojciechowski <jakub@alice.si>
2017-10-19 11:46:10 +0200

    Curated transfers

---
+++ contracts/alice/CuratedTransfer.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/token/ERC20.sol';
+import './TwoPhaseTransfers.sol';
+
+
+/**
+ * @title CuratedTransfers
+ * @dev The CuratedTransfers contract introduces a curator role that may block and resume any pending transfer.
+ */
+contract CuratedTransfers is TwoPhaseTransfers {
+
+    mapping(uint => bool) isBlocked;
+    address public curator;
+
+
+    modifier onlyCurator() {
+        require(msg.sender == curator);
+        _;
+    }
+
+
+    function CuratedTransfers(address _curator, address[] _proposers, address[] _validators)
+    TwoPhaseTransfers(_proposers, _validators) {
+        curator = _curator;
+    }
+
+
+    function blockTransfer(uint _transferId) onlyCurator {
+        isBlocked[_transferId] = true;
+    }
+
+
+    function resumeTransfer(uint _transferId) onlyCurator {
+        isBlocked[_transferId] = false;
+    }
+
+
+    function confirmTransfer(uint _transferId) onlyValidator {
+        require(!isBlocked[_transferId]);
+        super.confirmTransfer(_transferId);
+    }
+
+}
