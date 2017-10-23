d2759558700d02b48a9145f72d542cd47a492c67
jakub-wojciechowski <jakub@alice.si>
2017-10-23 19:41:54 +0200

    Vault integration contract(WIP)

---
+++ contracts/alice/Vault.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/token/ERC20.sol';
+import './CuratedWithWarnings.sol';
+
+
+/**
+ * @title Vault
+ * @dev The Vault contract is a container for token storage that implements extra security measures.
+ */
+contract Vault is CuratedWithWarnings {
+
+    function Vault(address[] _whistleblowers, address _curator, address[] _proposers, address[] _validators)
+        CuratedWithWarnings(_whistleblowers, _curator, _proposers, _validators) {
+    }
+
+}
