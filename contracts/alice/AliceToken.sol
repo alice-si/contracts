6dcd3646c54c25fed8b32e0ee2d755ad88e0fea9
jakub-wojciechowski <jakub@alice.si>
2017-12-20 20:31:13 +0100

    Alice token cleanup & upgrade

--- contracts/alice/AliceToken.sol
+++ contracts/alice/AliceToken.sol
@@ -2,13 +2,11 @@
 Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
 */
 
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-
-import "./Token.sol";
 
 contract AliceToken is StandardToken, Ownable {
     using SafeMath for uint256;
@@ -19,14 +17,14 @@ contract AliceToken is StandardToken, Ownable {
     string public version = 'ALT 1.0';
 
 
-    function mint(address _to, uint256 _value) onlyOwner {
+    function mint(address _to, uint256 _value) public onlyOwner {
         totalSupply = totalSupply.add(_value);
         balances[_to] =  balances[_to].add(_value);
 
         MintEvent(_to, _value);
     }
 
-    function destroy(address _from, uint256 _value) onlyOwner {
+    function destroy(address _from, uint256 _value) public onlyOwner {
         totalSupply = totalSupply.sub(_value);
         balances[_from] = balances[_from].sub(_value);
 
