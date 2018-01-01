6317ddb76524a08e485574fdcabb9160d51a178e
jakub-wojciechowski <jakub@alice.si>
2018-01-01 20:21:40 +0100

    Local ganache-cli for Travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,7 @@ output=$(nc -z localhost 8545; echo $?)
 if [ ! $trpc_running ]; then
   echo "Starting our own ganache node instance"
   # create 100 accounts for load tests
-  ganache-cli -a 100 -i 3 \
+  node ./node_modules/ganache-cli/build/cli.node.js -a 100 -i 3 \
   > /dev/null &
   trpc_pid=$!
 fi
