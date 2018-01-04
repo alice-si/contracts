17cdcee0106955917aa2787e98c704dd4eb625bd
jakub-wojciechowski <jakub@alice.si>
2018-01-04 17:32:35 +0100

    Bond & Coupon contracts

---
+++ contracts/alice/bonds/Coupon.sol
@@ -0,0 +1,36 @@
+/*
+Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
+*/
+
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+contract Coupon is StandardToken, Ownable {
+    using SafeMath for uint256;
+
+    string public name = "Alice Coupon";
+    uint8 public decimals = 2;
+    string public symbol = "ALT";
+    string public version = 'ALT 1.0';
+
+
+    function mint(address _to, uint256 _value) public onlyOwner {
+        totalSupply = totalSupply.add(_value);
+        balances[_to] =  balances[_to].add(_value);
+
+        MintEvent(_to, _value);
+    }
+
+    function burn(address _from, uint256 _value) public onlyOwner {
+        totalSupply = totalSupply.sub(_value);
+        balances[_from] = balances[_from].sub(_value);
+
+        BurnEvent(_from, _value);
+    }
+
+    event MintEvent(address indexed to, uint value);
+    event BurnEvent(address indexed from, uint value);
+}
---
+++ contracts/alice/bonds/ProjectWithBonds.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+import 'zeppelin-solidity/contracts/token/ERC20.sol';
+
+import "./Coupon.sol";
+import "../Project.sol";
+
+contract ProjectWithBonds is Project {
+
+    Coupon coupon;
+
+    /* This generates a public event on the blockchain that will notify clients */
+    event CouponIssuedEvent(address indexed to, uint value);
+    event CouponRepaidEvent(address indexed from, uint value);
+
+    function ProjectWithBonds(string _name) public
+        Project(_name) {
+        coupon = new Coupon();
+    }
+
+    function investFromWallet(ERC20 _token, uint _amount) public {
+        require(_token.transferFrom(msg.sender, address(this), _amount));
+        coupon.mint(msg.sender, _amount);
+        CouponIssuedEvent(msg.sender, _amount);
+    }
+
+
+    function unlockOutcome(ERC20 _token, string _name, uint _value) public {
+        require (msg.sender == judgeAddress);
+        require (_value <= total);
+
+        _token.transfer(beneficiaryAddress, _value);
+        total = total.sub(_value);
+
+        ImpactRegistry(IMPACT_REGISTRY_ADDRESS).registerOutcome(_name, _value);
+
+        OutcomeEvent(_name, _value);
+    }
+
+    function payBack(ERC20 _token, address account) public onlyOwner {
+        uint balance = getBalance(account);
+        if (balance > 0) {
+            _token.transfer(account, balance);
+            total = total.sub(accountBalances[account]);
+            ImpactRegistry(IMPACT_REGISTRY_ADDRESS).payBack(account);
+        }
+    }
+
+    function getCoupon() public view returns(Coupon) {
+        return coupon;
+    }
+}
